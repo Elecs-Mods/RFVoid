a985af03457dd6d8cc1bcf86b83c5e6c4ba6dae6
Elec332 <arnout1998@gmail.com>
2015-09-12 15:48:51 +0200

    Initial Code Commit

---
+++ src/main/java/elec332/rfvoid/init/BlockRegister.java
@@ -0,0 +1,15 @@
+package elec332.rfvoid.init;
+
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+/**
+ * Created by Elec332 on 24-2-2015.
+ */
+public class BlockRegister {
+    public static final BlockRegister instance = new BlockRegister();
+
+    public void init(){
+        //Normal block registering
+    }
+}
---
+++ src/main/java/elec332/rfvoid/proxies/CommonProxy.java
@@ -0,0 +1,7 @@
+package elec332.rfvoid.proxies;
+
+/**
+ * Created by Elec332 on 24-2-2015.
+ */
+public class CommonProxy {
+}
